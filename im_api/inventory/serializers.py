2a1aa29ec68119d5e9725355db06b37c46014c83
Samuel Takyi <s.k.takyi5@gmail.com>
2024-10-17 04:33:16 +1000

    updated-feature

---
+++ im_api/inventory/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from .models import Category, Product, Supplier, Inventory, InventoryLog, Customer, Order
+
+
+
+
+# Category Serializer
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+# Product Serializer
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+# Supplier Serializer
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+
+
+# Inventory Serializer
+class InventorySerializer(serializers.ModelSerializer):
+    product_name = serializers.StringRelatedField()  # To display product name
+
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+class InventoryLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryLog
+        fields = '__all__'
+
+
+# Customer Serializer
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+# Order Serializer
+class OrderSerializer(serializers.ModelSerializer):
+    customer = serializers.StringRelatedField()  # To display customer name
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
---
+++ im_api/inventory/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    CategoryViewSet,
+    ProductViewSet,
+    SupplierViewSet,
+    CustomerViewSet,
+    OrderViewSet,
+    InventoryViewSet,
+    InventoryLogViewSet
+)
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'suppliers', SupplierViewSet)
+router.register(r'customers', CustomerViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'inventory', InventoryViewSet)
+router.register(r'inventory-log', InventoryLogViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
