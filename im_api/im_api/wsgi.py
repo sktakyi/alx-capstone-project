16641de66af66167226a40f67bb195a518185053
Samuel Takyi <s.k.takyi5@gmail.com>
2024-10-20 23:41:30 +1000

    u-f

--- im_api/im_api/wsgi.py
+++ im_api/im_api/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Adjust the following line to include your project directory
+path = '/home/sktakyi/alx-capstone-project/im_api'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'im_api.settings'
+
+# Activate your virtual environment
+activate_this = '/home/sktakyi/alx-capstone-project/im_api/.venv/bin/activate_this.py'
+exec(open(activate_this).read(), {'__file__': activate_this})
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'im_api.settings')
-
 application = get_wsgi_application()
